043ed86ab0672a98b8fb4ddfb882e508dc658b55
rfong <rachefo@gmail.com>
2017-04-22 00:12:58 -0700

    shuffle random

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 var app = angular.module('StrumApp', []);
 
 app.controller('StrumCtrl', function($scope, $http) {
+  $scope.patternLength = 8;
   // true=down, false=up
   $scope.stroke = [true, false, true, false, true, false, true, false];
-
   // active or not
   $scope.pattern = [true, false, true, true, true, true, true, true];
+
+  $scope.refreshPattern = function() {
+    $scope.pattern = _.map(_.range($scope.patternLength), function(i) {
+        return Math.random() < 0.5;
+    });
+  };
+
 });
